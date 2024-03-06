fdfab05c28b61834a1bffa95d4f65b417f72b0da
Vansh Vasishtha <vansh19092002@gmail.com>
2024-03-06 21:53:39 +0530

    Some Progress in JS

--- build-server/script.js
+++ build-server/script.js
@@ -1,7 +1,21 @@
-const { exec } = require('child_process')
-const path = require('path')
+const { exec } = require("child_process");
+const path = require("path");
 
-async function init (){
-    console.log('Executing script.js')
-    
+async function init() {
+  console.log("Executing script.js");
+  const outDirPath = path.join(__dirname, "output");
+
+  const p = exec(`cd ${outDirPath} &&  npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  p.stdout.on("Error", function (data) {
+    console.log('Error',data.toString());
+  });
+
+  p.on('close', function(){
+    console.log('Build Complete');
+  })
 }
